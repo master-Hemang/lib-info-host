8e6a5f7ad5883b2da8bcade525c41266bfdc6c5a
Digvijay Anand <99537348+digvijayyanand@users.noreply.github.com>
2023-08-01 02:58:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,9 +5,9 @@ const path=require('path');
 var passport = require('passport');
 var authenticate = require('./authenticate');
 const cors = require('cors');
-
 const dotenv= require('dotenv');
 dotenv.config();
+
 
 // Loading routers
 const bookRouter = require('./routes/api/bookRouter');
@@ -30,7 +30,6 @@ app.use(function(req, res, next) {
 app.use(bodyParser.json());
 
 // DB config 
-//const mongoURI = process.env.DB;
 
 // Connect to mongo database
 mongoose.connect(process.env.DB)
@@ -45,7 +44,7 @@ app.use('/api/users',userRouter);
 app.use('/api/issues',issueRouter);
 
 // Serve static assets if in production
-/*if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === 'production') {
     // Set static folder
     app.use(express.static('client/build'));
   
@@ -53,7 +52,7 @@ app.use('/api/issues',issueRouter);
       res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
   }
-*/
-const port = process.env.PORT || 5000;
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, ()=> console.log(`Server started running on port ${port}`));
