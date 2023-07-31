d6670a80f1f70bd9a2e770e4eec3f0ccf27ebd21
Digvijay Anand <99537348+digvijayyanand@users.noreply.github.com>
2023-08-01 03:08:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,16 +16,6 @@ const issueRouter = require('./routes/api/issueRouter');
 const app= express();
 app.use(cors());
 
-app.use(function(req, res, next) {
-
-  res.header("Access-Control-Allow-Origin", "*");
-
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-
-  next();
-
-});
-
 // Bodyparser Middleware
 app.use(bodyParser.json());
 
